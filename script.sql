bf903c48bc21f82816bb02151bbd1e7144ddbac4
SirGlaucus <glaucusjoseph@gmail.com>
2022-04-07 21:37:01 -0400

    Cambiando el nombre de columna pelicula a titulo_pelicula para mayor claridad

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@ CREATE DATABASE peliculas;
 \c peliculas;
 
 -- 1.5. Crear las tablas
-CREATE TABLE peliculas (id INT PRIMARY KEY, pelicula VARCHAR(125), year_estreno INT, director VARCHAR(125));
+CREATE TABLE peliculas (id INT PRIMARY KEY, titulo_pelicula VARCHAR(125), year_estreno INT, director VARCHAR(125));
 CREATE TABLE reparto (pelicula_fk INT, actor VARCHAR(125), FOREIGN KEY (pelicula_fk) REFERENCES peliculas(id));
 
 -- 2. Cargar ambos archivos a su tabla correspondiente.
@@ -11,7 +11,7 @@ CREATE TABLE reparto (pelicula_fk INT, actor VARCHAR(125), FOREIGN KEY (pelicula
 \copy reparto FROM 'C:\Users\glauc\Documents\Programacion 2020\SENCEClases5\4/reparto.csv' csv;
 
 -- 3. Obtener el ID de la película “Titanic”.
-SELECT id AS titanic_id FROM peliculas WHERE pelicula = 'Titanic';
+SELECT id AS titanic_id FROM peliculas WHERE titulo_pelicula = 'Titanic';
 
 -- 4. Listar a todos los actores que aparecen en la película "Titanic".
 SELECT actor AS actores_titanic FROM reparto WHERE pelicula_fk = 2;
@@ -21,11 +21,11 @@ SELECT COUNT(pelicula_fk) AS cantidad_peliculas_harrison FROM reparto WHERE acto
 
 -- 6. Indicar las películas estrenadas entre los años 1990 y 1999 ordenadas por título de
 -- manera ascendente.
-SELECT pelicula, year_estreno FROM peliculas WHERE year_estreno BETWEEN 1990 AND 1999 ORDER BY pelicula ASC;
+SELECT titulo_pelicula, year_estreno FROM peliculas WHERE year_estreno BETWEEN 1990 AND 1999 ORDER BY titulo_pelicula ASC;
 
 -- 7. Hacer una consulta SQL que muestre los títulos con su longitud, la longitud debe ser
 -- nombrado para la consulta como “longitud_titulo”.
-SELECT pelicula, LENGTH(pelicula) AS longitud_titulo FROM peliculas;
+SELECT titulo_pelicula, LENGTH(titulo_pelicula) AS longitud_titulo FROM peliculas;
 
 -- 8. Consultar cual es la longitud más grande entre todos los títulos de las películas.
-SELECT MAX(LENGTH(pelicula)) FROM peliculas;
+SELECT MAX(LENGTH(titulo_pelicula)) FROM peliculas;
